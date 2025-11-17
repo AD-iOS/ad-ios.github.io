1b7d86386bc8f5231dfe58b60220f0e1855da621
iOS-GM <1107154510@qq.com>
2025-11-17 23:38:08 +0800

    Auto update_git 2025-11-17 23:38

--- update_dpkg.sh
+++ update_dpkg.sh
@@ -6,17 +6,6 @@ cd /var/mobile/ad
 dpkg-scanpackages -m . /dev/null > Packages
 xz -c Packages > Packages.xz
 bzip2 -c Packages > Packages.bz2
+gzip -c Packages > Packages.gz
 
-# 计算校验值
-md5_pkg=$(md5sum Packages | cut -d' ' -f1)
-size_pkg=$(wc -c < Packages)
-md5_xz=$(md5sum Packages.xz | cut -d' ' -f1)
-size_xz=$(wc -c < Packages.xz)
-md5_bz2=$(md5sum Packages.bz2 | cut -d' ' -f1)
-size_bz2=$(wc -c < Packages.bz2)
-
-# 提交更新
-# git add .
-# git commit -m "Auto-update $(date +'%Y-%m-%d %H:%M')"
-# git pull --rebase
-# git push origin main
+./update_git.sh
